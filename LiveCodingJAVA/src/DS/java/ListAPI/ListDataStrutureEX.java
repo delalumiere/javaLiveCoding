6eb3e2d5f29bd45d21c58d8e370859a4ea480655
yohanna <hancog@naver.com>
2022-11-07 15:33:56 +0900

    생활코딩 DS 6강

---
+++ LiveCodingJAVA/src/DS/java/ListAPI/ListDataStrutureEX.java
@@ -0,0 +1,17 @@
+package DS.java.ListAPI;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class ListDataStrutureEX {
+	
+	//JAVA는 List를 두개 지원한다.
+	// ArrayList 추가삭제 느림 
+	// LinkedList
+	// index 조회시 느림
+	
+	ArrayList arrayList= new ArrayList();
+	
+	LinkedList linkedList = new LinkedList();
+
+}
