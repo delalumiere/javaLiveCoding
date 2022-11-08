1efc1a44a4540b6a8d22db02c243c28969c9d7f2
yohanna <hancog@naver.com>
2022-11-08 11:18:48 +0900

    ArrayList - java 구현 4 - ADD 11강

--- LiveCodingJAVA/src/DS/java/list/linkedlist/implementation/ArrayList.java
+++ LiveCodingJAVA/src/DS/java/list/linkedlist/implementation/ArrayList.java
@@ -1,17 +1,24 @@
 package DS.java.list.linkedlist.implementation;
 
 public class ArrayList {
-//list data structure를 어떻게 만들것인가
+	//list data structure를 어떻게 만들것인가
 	private Object[] elementData = new Object[100];
 	private int size=0;
-	
+	public boolean addFirst(Object element) {
+		return add(0,element);
+	}
 	public boolean addLast(Object element) {
 		elementData[size] = element;
 		size++;
 		return true;
 	}
-	public boolean add(Object element) {
-	
+	public boolean add(int index, Object element) {
+		for(int i=size-1;i>=index;i--) {
+			elementData[i+1] = elementData[i];
+		}
+		elementData[index]=element;
+		size++;
+		return true;
 	}
 	
 }
--- LiveCodingJAVA/src/DS/java/list/linkedlist/implementation/Main.java
+++ LiveCodingJAVA/src/DS/java/list/linkedlist/implementation/Main.java
@@ -11,7 +11,7 @@ public class Main {
 		numbers.addLast(40);
 		numbers.add(1,15);
 		
-		
+		numbers.addFirst(5);
 	}
 
 }
