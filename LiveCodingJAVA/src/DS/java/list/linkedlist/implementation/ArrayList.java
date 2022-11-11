43c542a5e48f8a0e89a769b063ef9f2586cbf80c
yohanna <hancog@naver.com>
2022-11-11 19:04:38 +0900

    ArrayList - java 구현 9 - iterator next hasNext

--- LiveCodingJAVA/src/DS/java/list/linkedlist/implementation/ArrayList.java
+++ LiveCodingJAVA/src/DS/java/list/linkedlist/implementation/ArrayList.java
@@ -73,5 +73,32 @@ public class ArrayList {
 		}
 		return -1; //없으면 -1을 return 해준다
  	}
+
+	public ListIterator listIterator() {
+//         리턴할 데이터타입  메소드 이름  		
+		return new ListIterator();
+	}
+	
+	class ListIterator {
+		private int nextIndex =0;
+		public Object next() {
+			//arraylist의 elementData에 있는 데이터를 리턴받아야됨 
+			// 인덱스 추출 하는 방법 next호출할때마다 index값을 1씩 증가시킴
+	/*		Object returnData = elementData[nextIndex]; //처음에는 nextIndex 0
+			nextIndex++; //인덱스값을 1 증가시킴
+			return returnData; // 값을 증가시켜 리턴함
+		*/
+			return elementData[nextIndex++]; //nextIndex 값이 100에 도달하면 ArrayIndex outbound exception에러발생
+		}
+ // nextIndex 값을 실행값을 간단하게	
+			//hasNext구현
+	        public boolean hasNext() {
+	            // nextIndex가 엘리먼트의 숫자보다 적다면 next를 이용해서 탐색할 엘리먼트가 존재하는 것이기 때문에 true를 리턴합니다. 
+	            return nextIndex < size();
+	        }
+	         
+		
+		//
+	}
 	
 }
--- LiveCodingJAVA/src/DS/java/list/linkedlist/implementation/Main.java
+++ LiveCodingJAVA/src/DS/java/list/linkedlist/implementation/Main.java
@@ -1,7 +1,6 @@
 package DS.java.list.linkedlist.implementation;
 
 
-
 public class Main {
 	public static void main(String[] args) {
 		ArrayList numbers = new ArrayList();
@@ -10,7 +9,7 @@ public class Main {
 		numbers.addLast(30);
 		numbers.addLast(40);
 		
-		System.out.println(numbers);
+//		System.out.println(numbers);
 // 		numbers.removeLast();
 //		numbers.remove(1);
 //		System.out.println(numbers.remove(1));
@@ -20,8 +19,28 @@ public class Main {
 //		System.out.println(numbers.get(3));
 		// size() method 구현 이유 > 캡슐레이션으로 외부에서 값을 변경할 수 없도록 하기 위해서 
 //		System.out.println(numbers.size()); 
-		System.out.println(numbers.indexOf(50));
+//		System.out.println(numbers.indexOf(50));
 		
+//Iterator 를 사용해서 List객체를 처리할 수 있다. 
+// data type은 numbers의 클래스를 지정한다. Listiterator
+		ArrayList.ListIterator li = numbers.listIterator(); 
+//numbers ArrayList 객체이름
+//ListerIterator는 객체의 타입 
+//ArrayList.ListerIterator는 ArrayList안에 포함될 ListIterator
+//listIterator()는 메소드임	
+//	System.out.println(li.next());
+//	System.out.println(li.next());
+//	System.out.println(li.next());
+//	System.out.println(li.next());
+//	System.out.println(li.next());
+	//처음호출하면 10 그다음 20 ... 
+		
+	//li.next() 자동화
+	//while(조건)	
+		while(li.hasNext()) {
+			System.out.println(li.next());
+		}
+	
 	}
 
 }
